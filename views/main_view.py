027487d52bf6d5f3836bd4b7630de98a7dfda179
hshkuber <hshkuber@vmkuber.myguest.virtualbox.org>
2023-12-19 19:19:30 +0900

    error test

--- views/main_view.py
+++ views/main_view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, current_app
 from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix = '/')
@@ -9,6 +9,7 @@ def ping() :
 
 @bp.route('/')
 def index() : 
-    3/0
+    
+    current_app.logger.info("INFO 레벨로 출력")
     # question : question_view blueprint, qlist : routing func 
     return redirect(url_for('question.qlist'))
