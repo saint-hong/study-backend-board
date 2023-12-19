4b0d53fe0816413c5d611873e2f7bbbd85738ca4
hshkuber <hshkuber@vmkuber.myguest.virtualbox.org>
2023-12-19 18:34:21 +0900

    test zero error

--- views/main_view.py
+++ views/main_view.py
@@ -4,12 +4,11 @@ from werkzeug.utils import redirect
 bp = Blueprint('main', __name__, url_prefix = '/')
 
 @bp.route('/ping', methods = ['GET'])
-def ping() : 
-    
+def ping() :     
     return 'pong'
 
 @bp.route('/')
 def index() : 
-    
+    3/0
     # question : question_view blueprint, qlist : routing func 
     return redirect(url_for('question.qlist'))
